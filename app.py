950b749e826c97bca1d35cc5dbed228df553cb78
Tejalwahadane <160157255+Tejalwahadane@users.noreply.github.com>
2025-02-11 21:21:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def model_prediction(test_image):
     image=tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr=np.array([input_arr])
-    predictions=model_predict(input_arr)
+    predictions=model.predict(input_arr)
     return np.argmax(predictions)
 
 #Sidebar
